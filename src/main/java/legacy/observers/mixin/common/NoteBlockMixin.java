1432024299baf3ff427e556b438b30760fe70249
Space Walker <spacedoesrs@gmail.com>
2024-08-25 12:23:36 +0200

    update to mc 1.11.2

--- src/main/java/legacy/observers/mixin/common/NoteBlockMixin.java
+++ src/main/java/legacy/observers/mixin/common/NoteBlockMixin.java
@@ -8,13 +8,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import legacy.observers.world.ModGamerules;
-import legacy.observers.world.ModWorld;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.NoteBlock;
 import net.minecraft.block.state.BlockState;
 import net.minecraft.entity.living.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.InteractionHand;
@@ -32,7 +30,7 @@ public class NoteBlockMixin {
 			target = "Lnet/minecraft/block/entity/NoteBlockBlockEntity;powered:Z"
 		)
 	)
-	private void triggerObservers(BlockState state, World world, BlockPos pos, Block neighborBlock, CallbackInfo ci) {
+	private void triggerObservers(BlockState state, World world, BlockPos pos, Block neighborBlock, BlockPos neighborPos, CallbackInfo ci) {
 		triggerObservers(world, pos);
 	}
 
@@ -44,13 +42,13 @@ public class NoteBlockMixin {
 			target = "Lnet/minecraft/block/entity/NoteBlockBlockEntity;tunePitch()V"
 		)
 	)
-	private void triggerObservers(World world, BlockPos pos, BlockState state, PlayerEntity player, InteractionHand hand, ItemStack stack, Direction face, float dx, float dy, float dz, CallbackInfoReturnable<Boolean> cir) {
+	private void triggerObservers(World world, BlockPos pos, BlockState state, PlayerEntity player, InteractionHand hand, Direction face, float dx, float dy, float dz, CallbackInfoReturnable<Boolean> cir) {
 		triggerObservers(world, pos);
 	}
 
 	private void triggerObservers(World world, BlockPos pos) {
 		if (ModGamerules.noteBlocksTriggerObservers) {
-			((ModWorld)world).updateObservers(pos, (Block)(Object)this);
+			world.updateObservers(pos, (Block)(Object)this);
 		}
 	}
 }
--- src/main/java/legacy/observers/world/SetBlockFlags.java
+++
@@ -1,8 +0,0 @@
-package legacy.observers.world;
-
-public class SetBlockFlags {
-
-	public static final int UPDATE_CLIENTS        = 0b00010;
-	public static final int SKIP_UPDATE_OBSERVERS = 0b10000;
-
-}
